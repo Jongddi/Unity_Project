50cdd6ec17cd7c835632bcfc5a530970d7d8392e
Jongddi <75599079+Jongddi@users.noreply.github.com>
2021-01-23 18:17:34 +0900

    사운드 추가
    사운드 추가

--- 2D_Platformer/Assets/Script/PlayerMove.cs
+++ 2D_Platformer/Assets/Script/PlayerMove.cs
@@ -5,13 +5,20 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour
 {
     public GameManager gameManager;
+    public AudioClip audioJump;
+    public AudioClip audioAttack;
+    public AudioClip audioDamaged;
+    public AudioClip audioItem;
+    public AudioClip audioDie;
+    public AudioClip audioFinish;
     public float maxSpeed;
     public float jumpPower;
+
     Rigidbody2D rigid;
     SpriteRenderer spriteRenderer;
     Animator anim;
     CapsuleCollider2D capsuleCollider;
-
+    AudioSource audioSource;
 
     void Awake()
     {
@@ -19,6 +26,7 @@ public class PlayerMove : MonoBehaviour
         spriteRenderer = GetComponent<SpriteRenderer>();
         anim = GetComponent<Animator>();
         capsuleCollider = GetComponent<CapsuleCollider2D>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     void Update() //단발적 입력
@@ -28,12 +36,15 @@ public class PlayerMove : MonoBehaviour
         {
             rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
             anim.SetBool("isJumping", true);
+
+            //Sound
+            PlaySound("JUMP");
         }
 
+        //Stop Speed
         if (Input.GetButtonUp("Horizontal"))
         {
             //rigid.velocity.nomalized //nomalized 벡터크기를 1로 만든 상태
-            //Stop Speed
             rigid.velocity = new Vector2(rigid.velocity.normalized.x * 0.5f, rigid.velocity.y);
         }
 
@@ -92,19 +103,30 @@ public class PlayerMove : MonoBehaviour
         if (collision.gameObject.tag == "Enemy")
 		{
             //Debug.Log("플레이어가 맞았습니다!");
-            
+
             //Attack
             if (rigid.velocity.y < 0 && transform.position.y > collision.transform.position.y)
-			{
+            {
                 OnAttack(collision.transform);
-			}
+
+                //Sound
+                PlaySound("ATTACK");
+            }
             else //Damaged
+            {
                 OnDamaged(collision.transform.position);
+
+                //Sound
+                PlaySound("DAMAGED");
+            }
         }
 
         else if (collision.gameObject.tag == "Spikes") //가시 충돌이벤트 분리
         {
             OnDamaged(collision.transform.position);
+
+            //Sound
+            PlaySound("DAMAGED");
         }
     }
 
@@ -133,13 +155,19 @@ public class PlayerMove : MonoBehaviour
 
             // Deactive Item
             collision.gameObject.SetActive(false);
-		}
+
+            //Sound
+            PlaySound("ITEM");
+        }
 
         else if(collision.gameObject.tag == "Finish")
 		{
             // Next Stage
             gameManager.NextStage();
-		}
+
+            //Sound
+            PlaySound("FINISH");
+        }
 	}
 
 	void OnAttack(Transform enemy)
@@ -185,6 +213,7 @@ public class PlayerMove : MonoBehaviour
         spriteRenderer.color = new Color(1, 1, 1, 1);
 	}
 
+    //사망 이펙트
    public void OnDie()
 	{
         //Sprite Alpha
@@ -200,8 +229,36 @@ public class PlayerMove : MonoBehaviour
         rigid.AddForce(Vector2.up * 5, ForceMode2D.Impulse);
     }
 
+    //원위치
     public void VelocityZero()
 	{
         rigid.velocity = Vector2.zero;
 	}
+
+    //Sound Effect
+    void PlaySound(string action)
+    {
+        switch (action)
+        {
+            case "JUMP":
+                audioSource.clip = audioJump;
+                break;
+            case "ATTACK":
+                audioSource.clip = audioAttack;
+                break;
+            case "DAMAGED":
+                audioSource.clip = audioDamaged;
+                break;
+            case "ITEM":
+                audioSource.clip = audioItem;
+                break;
+            case "DIE":
+                audioSource.clip = audioDie;
+                break;
+            case "FINISH":
+                audioSource.clip = audioFinish;
+                break;
+        }
+        audioSource.Play();
+    }
 }
