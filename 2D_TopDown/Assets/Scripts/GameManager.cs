85b2e356696adb56a39088856feaed1164e02ea4
Jongddi <75599079+Jongddi@users.noreply.github.com>
2021-01-27 18:57:01 +0900

    오브젝트 관리 및 대화시스템 구현
    Dictionary함수를 이용하여 오브젝트별 대화, 스프라이트 분리 / 오브젝트 마다 Key값을 주어 구별
    Add함수를 이용 각 Key마다 대화창에서 다른 문장들이 출력되어 나오게 구현
    NPC들과 대화시에만 각 NPC초상화가 나오도록 Split함수를 사용하여 분리 후 parse를 이용하여 문자,스프라이트int값 구별 구현

--- 2D_TopDown/Assets/Scripts/GameManager.cs
+++ 2D_TopDown/Assets/Scripts/GameManager.cs
@@ -6,24 +6,50 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    public TalkManager talkManager;
     public GameObject talkPanel;
+    public Image portraitImg;
     public Text talkText;
     public GameObject scanObject;
     public bool isAction;
+    public int talkIndex;
 
     public void Action(GameObject scanObj)
 	{
-        if (isAction)//Exit Action
-        {
-            isAction = false;
-        }
-		else//Enter Action
-        {
-            isAction = true;
-            scanObject = scanObj;
-            talkText.text = "이것의 이름은 " + scanObject.name + "이라고 한다.";
-        }
+       
+	
+       isAction = true;
+       scanObject = scanObj;
+       ObjectData objData = scanObject.GetComponent<ObjectData>();
+       Talk(objData.id, objData.isNpc);
+       
         talkPanel.SetActive(isAction);
     }
 
+    void Talk(int id, bool isNpc)
+	{
+        string talkData = talkManager.GetTalk(id, talkIndex);
+
+        if(talkData == null)
+		{
+            isAction = false;
+            talkIndex = 0;
+            return;
+		}
+
+        if(isNpc)
+		{
+            talkText.text = talkData.Split(':')[0];    //Split 구분자를 통하여 배열로 나눠주는 문자열 함수
+
+            portraitImg.sprite = talkManager.GetPortrait(id, int.Parse (talkData.Split(':')[1])); //parse 문자열을 해당 타입으로 변화해주는 함수
+            portraitImg.color = new Color(1, 1, 1, 1);
+		}
+        else
+        {
+            talkText.text = talkData;
+            portraitImg.color = new Color(1, 1, 1, 0);
+        }
+        isAction = true;
+        talkIndex++;
+    }
 }
---
+++ 2D_TopDown/Assets/Scripts/TalkManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TalkManager : MonoBehaviour
+{
+    Dictionary<int, string[]> talkData; //Key+Value
+    Dictionary<int, Sprite> portraitData;
+
+    public Sprite[] portraitArr;
+
+    void Awake()
+    {
+        talkData = new Dictionary<int, string[]>();
+        portraitData = new Dictionary<int, Sprite>();
+        GenerateData();
+    }
+
+
+    void GenerateData()
+    {
+        talkData.Add(1000, new string[] { "어서와:0", "여긴처음이지....?:1" });
+        talkData.Add(2000, new string[] { "들어올 땐 마음대로지만...:0", "나갈땐 아니란다..:1" });
+        talkData.Add(100, new string[] {"평범한 나무상자네...."});
+        talkData.Add(200, new string[] { "누군가 사용한 듯한 책상이네.." });
+
+        portraitData.Add(1000 + 0, portraitArr[0]);
+        portraitData.Add(1000 + 1, portraitArr[1]);
+        portraitData.Add(1000 + 2, portraitArr[2]);
+        portraitData.Add(1000 + 3, portraitArr[3]);
+        portraitData.Add(2000 + 0, portraitArr[4]);
+        portraitData.Add(2000 + 1, portraitArr[5]);
+        portraitData.Add(2000 + 2, portraitArr[6]);
+        portraitData.Add(2000 + 3, portraitArr[7]);
+    }
+
+    public string GetTalk(int id, int talkIndex)
+	{
+        if (talkIndex == talkData[id].Length)
+            return null;
+        else
+            return talkData[id][talkIndex];
+	}
+
+    public Sprite GetPortrait(int id, int portaraitIndex)
+	{
+        return portraitData[id + portaraitIndex];
+	}
+}
