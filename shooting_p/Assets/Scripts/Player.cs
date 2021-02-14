ea1d1185c1255ec2dc76efc4ffa9e2dd9eba05d9
Jongddi <75599079+Jongddi@users.noreply.github.com>
2021-02-14 19:31:34 +0900

    플레이어 총알 생성
    게임이 실행된 후 플레이어가 총알을 지속적으로 발사하도록 구현함 자세한 코드 설명은 주석과 코멘트를 달아둠

--- shooting_p/Assets/Scripts/Player.cs
+++ shooting_p/Assets/Scripts/Player.cs
@@ -4,26 +4,38 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float speed;
+    //Bool
     public bool isTouchTop;
     public bool isTouchBottom;
     public bool isTouchRight;
     public bool isTouchLeft;
 
+    //Float
+    public float speed;
+    public float power;
+    public float curShotDelay;
+    public float maxShotDelay;
+
+    //Class,GetComponent
     Animator anim;
 
-    void Awake()
+    //Prefab
+    public GameObject bulletObjA;
+    public GameObject bulletObjB;
+
+    void Awake()        //프로그램 실행 전 다른 클래스에서 오는 컴포넌트 초기화
     {
         anim = GetComponent<Animator>();
-
     }
 
-    void Update()
+    void Update()       //초당 N회 실행
     {
         Move();
+        Fire();
+        Reload();
     }
 
-    void Move()
+    void Move()             //플레이어 이동 함수
     {
         float h = Input.GetAxisRaw("Horizontal");
         if ((isTouchRight && h == 1) || (isTouchLeft && h == -1))               //경계값을 넘지 못하도록 고정
@@ -44,22 +56,57 @@ public class Player : MonoBehaviour
         }
     }
 
+    void Fire()
+	{
+        if (curShotDelay < maxShotDelay)
+            return;
+
+        switch(power)               //플레이어의 강화단계에 따라 총알 오브젝트의 포지션,로테이션 변동
+		{
+            case 1:
+                GameObject bullet = Instantiate(bulletObjA, transform.position, transform.rotation);                //Instantiate(복제할 오브젝트 ,생성될 위치 ,생성될 방향)
+                Rigidbody2D rigid = bullet.GetComponent<Rigidbody2D>();                                                     //총알 오브젝트의 리지드바디호출
+                rigid.AddForce(Vector2.up * 10, ForceMode2D.Impulse);                                                          //AddForce(힘을받아 날아갈 방향*속도, 힘의 정도) impulse = 순간적인 힘 == 한번 받은 힘에 의해 일정한 속도로 고정
+                break;
+            case 2:
+                GameObject bulletR= Instantiate(bulletObjA, transform.position+Vector3.right * 0.1f, transform.rotation);
+                GameObject bulletL= Instantiate(bulletObjA, transform.position+ Vector3.left * 0.1f, transform.rotation);
+                Rigidbody2D rigidR = bulletR.GetComponent<Rigidbody2D>();
+                Rigidbody2D rigidL = bulletL.GetComponent<Rigidbody2D>();
+                rigidR.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+                rigidL.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+                break;
+            case 3:
+                GameObject bulletB = Instantiate(bulletObjB, transform.position, transform.rotation);
+                Rigidbody2D rigidB = bulletB.GetComponent<Rigidbody2D>();
+                rigidB.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+                break;
+        }
+
+        curShotDelay = 0;
+	}
+
+    void Reload()
+	{
+        curShotDelay += Time.deltaTime;
+	}
+
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Border")
         {
             switch (collision.gameObject.name)
             {
-                case "Top":
+                case "B_Top":
                     isTouchTop = true;
                     break;
-                case "Bottom":
+                case "B_Bottom":
                     isTouchBottom = true;
                     break;
-                case "Right":
+                case "B_Right":
                     isTouchRight = true;
                     break;
-                case "Left":
+                case "B_Left":
                     isTouchLeft = true;
                     break;
             }
@@ -71,16 +118,16 @@ public class Player : MonoBehaviour
         {
             switch (collision.gameObject.name)
             {
-                case "Top":
+                case "B_Top":
                     isTouchTop = false;
                     break;
-                case "Bottom":
+                case "B_Bottom":
                     isTouchBottom = false;
                     break;
-                case "Right":
+                case "B_Right":
                     isTouchRight = false;
                     break;
-                case "Left":
+                case "B_Left":
                     isTouchLeft = false;
                     break;
             }
