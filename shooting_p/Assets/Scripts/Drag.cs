7b369c53faff90292f8a3bebc9d2ea06ef1347a2
Jongddi <75599079+Jongddi@users.noreply.github.com>
2021-02-22 16:56:19 +0900

    플레이어 이동 로직 추가/모바일
    모바일에서는 키보드방향키조작이 아닌 플레이어 오브젝트를 직접 이동시키는 방식을 구현하기 위해 OnMouse()함수를 이용하여 로직을 만듬, 플레이어의 콜라이더에 마우스가 클릭을 유지하고 있을 때 마우스 이동위치로 오브젝트가 옮겨지는 방식임

---
+++ shooting_p/Assets/Scripts/Drag.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Drag : MonoBehaviour
+{
+
+    private Vector3 m_Offset;
+    private float m_Coordinate;
+
+    void OnMouseDown()
+    {
+        m_Coordinate = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
+        m_Offset = gameObject.transform.position - GetMouseWorldPosition();
+    }
+
+    void OnMouseDrag()
+    {
+        transform.position = GetMouseWorldPosition() + m_Offset;
+    }
+
+    Vector3 GetMouseWorldPosition()
+    {
+        Vector3 mousePoint = Input.mousePosition;
+        mousePoint.z = m_Coordinate;
+
+        return Camera.main.ScreenToWorldPoint(mousePoint);
+    }
+}
